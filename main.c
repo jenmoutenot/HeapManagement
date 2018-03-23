72f26a4147db64598b358559ba58c160dba9f75d
Jennifer Moutenot <jenmoutenot4@gmail.com>
2018-03-23 10:52:08 -0400

    hello

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+#include "allocator.h"
+
+
+
+int main()
+{
+ //Create an array of BLOCK_COUNT ENTRIES
+ // for ITERATIONS iterations
+   //pick a random block
+   // if that block is allocated, free it
+   // otherwise allocate BLOCK_SIZE bytes and assign it to the blocks 
+
+
+
+
+
+
+return 0;
+
+}
