67777cc2f0bd4d0f7a97c4a58cb699c880e99120
Jennifer Moutenot <jenmoutenot4@gmail.com>
2018-03-27 14:36:03 -0400

    hello

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include "stdio.h"
-#include "allocator.h"
-
-
-
+//#include "allocator.h"
+#include "allocator.c"
 int main()
 {
  //Create an array of BLOCK_COUNT ENTRIES
@@ -11,11 +9,15 @@ int main()
    // if that block is allocated, free it
    // otherwise allocate BLOCK_SIZE bytes and assign it to the blocks 
 
+ int *p=(int)MyMalloc(100*sizeof(int));
+ char *q=(char)MyMalloc(250*sizeof(char));
+ int *r=(int)MyMalloc(1000*sizeof(int));
+ MyFree(p);
+ char *w=(char)MyMalloc(700);
+ MyFree(r);
+ int *k=(int)MyMalloc(500*sizeof(int));
+ printf("Allocation and deallocation is done successfully!");
 
-
-
-
-
-return 0;
+// return 0;
 
 }
