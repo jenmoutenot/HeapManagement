babed09ccb3781a361c05248936c42db0b4c0388
Jennifer Moutenot <jenmoutenot4@gmail.com>
2018-03-27 18:15:36 -0400

    hi

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 //#define mallocmem(p) malloc(p)
 //#endif
 
-#define BLOCK_COUNT 10
+#define BLOCK_COUNT 21
 #define BLOCK_SIZE 8
 #define ITERATIONS 10
 
@@ -22,7 +22,10 @@ int main()
 {
   int ARRAY[BLOCK_COUNT];
     for (int i = 0; i < ITERATIONS; i++)
-        
+    {
+      int r = rand() % BLOCK_SIZE;
+      
+    }   
   return 0;
 
 }
