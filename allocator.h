a0638a84f92180e4fdf19cf097c55ead07364206
Jennifer Moutenot <jenmoutenot4@gmail.com>
2018-03-27 19:05:49 -0400

    hello

--- allocator.h
+++ allocator.h
@@ -2,24 +2,25 @@
 // Jen Moutenot
 // CS 366
 
-#include<stdio.h>
-#include<stddef.h>
+#include "stdio.h"
+#include "stddef.h"
 
-//#ifndef MYWAY
-//#define MYWAY
+#ifndef MYWAY
+#define MYWAY
 
 char memory[20000];
 
 struct block{
- size_t size;
- int free;
- struct block *next;
-};
+  int size;
+  int free;
+  struct block *next;
+} blocks;
 
 //struct block *freeList=(void*)memory;
 
 void initialize();
-//void split(struct block *fitting_slot,size_t size);
-void *MyMalloc(size_t noOfBytes);
-//void merge();
-void MyFree(void* ptr);
+void *my_malloc(int noOfBytes);
+void merge();
+void my_free(void* ptr);
+
+#endif
