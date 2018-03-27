80211b9d3e6f2a7c8910045dc441e08a6c595479
Jennifer Moutenot <jenmoutenot4@gmail.com>
2018-03-27 17:39:58 -0400

    hi

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 // CS 366
 
 #include "stdio.h"
-//#include "allocator.h"
-#include "allocator.c"
+#include "allocator.h"
 
 
 //#ifdef MYWAY
@@ -27,15 +26,15 @@ int main()
    // if that block is allocated, free it
    // otherwise allocate BLOCK_SIZE bytes and assign it to the blocks 
 
- int *p=(int)MyMalloc(100*sizeof(int));
- char *q=(char)MyMalloc(250*sizeof(char));
- int *r=(int)MyMalloc(1000*sizeof(int));
- MyFree(p);
- char *w=(char)MyMalloc(700);
- MyFree(r);
- int *k=(int)MyMalloc(500*sizeof(int));
+// int *p=(int)MyMalloc(100*sizeof(int));
+// char *q=(char)MyMalloc(250*sizeof(char));
+// int *r=(int)MyMalloc(1000*sizeof(int));
+// MyFree(p);
+// char *w=(char)MyMalloc(700);
+// MyFree(r);
+// int *k=(int)MyMalloc(500*sizeof(int));
  printf("Allocation and deallocation is done successfully!");
 
-// return 0;
+ return 0;
 
 }
