331e0be261aa857f41c2950d06fe90b487d04826
Jennifer Moutenot <jenmoutenot4@gmail.com>
2018-03-29 18:48:02 -0400

    hi

--- allocator.h
+++ allocator.h
@@ -5,12 +5,21 @@
 #include "stdio.h"
 #include "stddef.h"
 
+#ifdef MYWAY
+#define get_mem  my_alloc
+#define free_mem my_free
+#define BLOCK_SIZE 10
+#endif
+
 #ifndef MYWAY
-#define MYWAY
+#define get_mem malloc
+#define free_mem free
+#define BLOCK_SIZE sizeof(struct sBlock)
+#endif
 
 char memory[20000];
 
-struct block{
+struct sHeap{
   int size;
   int free;
   struct block *next;
@@ -22,5 +31,3 @@ void initialize();
 void *get_mem(int noOfBytes);
 //void merge();
 void my_free(void* ptr);
-
-#endif
