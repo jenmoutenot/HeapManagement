d7cf3790f44d29186068394ae97f5c3edd834105
Jennifer Moutenot <jenmoutenot4@gmail.com>
2018-03-29 18:55:12 -0400

    hi

--- main.c
+++ main.c
@@ -10,29 +10,21 @@
 #define BLOCK_SIZE 8
 #define ITERATIONS 200000000
 
-//#ifdef MYWAY
-//#define freemem(p) my_free(p)  //freemem = alias name
-//#define mallocmem(p) my_malloc(p)
-//#else
-//#define freemem(p) free(p)
-//#define mallocmem(p) malloc(p)
-//#endif
-
 int main()
 {
-  struct block list_array[BLOCK_COUNT];
+  struct tHeap sBlock[BLOCK_COUNT];
   srand(time(NULL));
   for (int i = 0; i < ITERATIONS; i++)
   {
       int num = rand() % BLOCK_COUNT;
       printf("%i\n", num);
-      if(list_array[num] == NULL);
+      if(sBlock[num] == NULL);
       {
         get_mem(); 
       }
       else
       { 
-        my_free();
+        free_mem();
       }
   }   
   return 0;
